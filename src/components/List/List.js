f32cf5e322ab727f584343662914be2e9ea47ccb
Logvina <irinlogvina9@gmail.com>
2021-12-11 21:15:09 +0200

    1

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,39 @@
 import React from "react";
 
+import s from "./List.module.css";
+
+import deleteIcon from "../../images/svg/delete.svg";
+
 function List() {
   return (
-    <div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+    <div className={s.listContainer}>
+      <table className={s.table}>
+        <thead className={s.headerTable}>
+          <tr>
+            <th className={s.tableTh}>Дата</th>
+            <th className={s.tableTh}>Описание</th>
+            <th className={s.tableTh}>Категория</th>
+            <th className={s.tableTh}>Сумма</th>
+            <th className={s.tableTh}></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={s.tr}>
+            <td>date</td>
+            <td>description</td>
+            <td>category</td>
+            <td>summa</td>
+            <td>
+              <button
+                type="button"
+                className={s.deleteBtn}
+              >
+                <img className={s.icon} src={deleteIcon} alt="Delete icon" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
