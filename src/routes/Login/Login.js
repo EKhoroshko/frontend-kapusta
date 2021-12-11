194ebdbf51f3ac0da5944f993169b7943248cd71
KasyanAndrey <andreykasyan88@gmail.com>
2021-12-11 06:10:32 +0200

    creat cmpnt LoginForm, add css/chng route Login

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,8 +1,32 @@
 import React from "react";
-import css from "./Login.module.css";
+import s from "./Login.module.css";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 function Login() {
-  return <div>Login page</div>;
+  return (
+    <div className={s.loginPage}>
+      <div className={s.titleBox}>
+        <h1 className={s.title}>
+          Kapu<span></span>ta
+        </h1>
+        <p className={s.titleDescription}>Smart finance</p>
+      </div>
+      <div className={s.formBox}>
+        <p className={s.descriptionA}>
+          Вы можете авторизоваться с помощью Google Account:
+        </p>
+        <button className={s.button} type="button">
+          <span></span>
+          Google
+        </button>
+        <p className={s.descriptionB}>
+          Или зайти с помощью e-mail и пароля, предварительно
+          зарегистрировавшись:
+        </p>
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
