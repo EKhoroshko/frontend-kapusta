bca5c15bfe65b1314b1e0569978e85f7a465ed2b
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-12-11 16:00:53 +0200

    test

---
+++ src/pages/ReportPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const ReportPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default ReportPage;
