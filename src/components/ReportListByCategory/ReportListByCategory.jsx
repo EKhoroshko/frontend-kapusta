3b9a6189a7d3a383add85873c67d2b591876eec2
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-12-11 20:21:53 +0200

    add components

---
+++ src/components/ReportListByCategory/ReportListByCategory.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReportItemByCategory from "../ReportItemByCategory";
+
+const ReportListByCategory = () => {
+  return (
+    <div>
+      <ul>
+        <li key={}>
+          <button type='button' onClick={}>
+            <ReportItemByCategory />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ReportListByCategory;
--- src/pages/ReportPage/index.jsx
+++ src/pages/ReportPage/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import ReportListByCategory from "../../components/ReportListByCategory";
+import DetailsBalance from "../../components/DetailsBalance";
 
 const ReportPage = () => {
   return (
     <>
       <Header />
+      {/* <ButtonBackToPreviousPage />
+      <СurrentPeriod />
+      <CurrentBalance /> */}
+      <DetailsBalance />
+      <ReportListByCategory />
+      {/* <ReportListChart /> */}
     </>
   );
 };
