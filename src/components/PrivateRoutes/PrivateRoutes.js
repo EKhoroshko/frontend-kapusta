de8b211cb4a49a61d60bfd337907eebf1689d595
Dmytro Bykanov <75586266+DimByl@users.noreply.github.com>
2021-12-12 19:44:48 +0200

    fix

--- src/components/PrivateRoutes/PrivateRoutes.js
+++ src/components/PrivateRoutes/PrivateRoutes.js
@@ -3,13 +3,13 @@ import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getIsLoggedIn } from "../../redux/auth/selectors";
 
-const PrivateRoute = ({component: Component, isLoggedIn, redirectTo, ...routeProps}) => {
+const PrivateRoute = ({component: Component, ...routeProps}) => {
     const isLoggedIn = useSelector(getIsLoggedIn);
     return (
         <Route
             {...routeProps}
             render={props =>
-                isLoggedIn ? <Component {...props} /> : <Redirect to={redirectTo} />
+                isLoggedIn ? <Component {...props} /> : <Redirect to="/home" />
             }
         />
     )
