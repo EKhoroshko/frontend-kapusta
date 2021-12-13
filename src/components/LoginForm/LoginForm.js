6eff9f826c5ad2c3efe37a9023788bc1bb389256
EKhoroshko <xapoxa14@gmail.com>
2021-12-13 16:02:12 +0200

    Start Logic

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,9 +1,45 @@
 import s from "./LoginForm.module.css";
+import Button from "../Button/Button";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const LoginForm = () => {
+  const dispatch = useDispatch();
+  const [mail, setMail] = useState("");
+  const [pass, setPass] = useState("");
+  const [type, setType] = useState("");
+
+  console.log(type);
+
+  const waitCheck = (e) => {
+    const { name, value } = e.currentTarget;
+    if (name === "email") {
+      setMail(value);
+    } else {
+      setPass(value);
+    }
+  };
+
+  const handlSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      type: type ? "Войти" : "Регистрация",
+      mail,
+      pass,
+    };
+    console.log("type", user);
+    setMail("");
+    setPass("");
+    setType("");
+  };
+
+  const checkType = (e) => {
+    setType(e.target.outerText);
+  };
+
   return (
     <div className={s.wrapper}>
-      <form className={s.form} onSubmit>
+      <form className={s.form} onSubmit={handlSubmit}>
         <label className={s.label}>
           <p className={s.description}>Электронная почта:</p>
           <input
@@ -11,8 +47,8 @@ const LoginForm = () => {
             type="text"
             name="email"
             required
-            value
-            onChange
+            value={mail}
+            onChange={waitCheck}
           />
         </label>
         <label className={s.label}>
@@ -22,19 +58,15 @@ const LoginForm = () => {
             type="password"
             name="password"
             required
-            value
-            onChange
+            value={pass}
+            onChange={waitCheck}
             autoComplete="off"
           />
         </label>
 
         <div className={s.formButton}>
-          <button className={s.button} type="submit">
-            Войти
-          </button>
-          <button className={s.button} type="submit">
-            Регистрация
-          </button>
+          <Button text="Войти" lassName={s.button} onClick={checkType} />
+          <Button text="Регистрация" lassName={s.button} onClick={checkType} />
         </div>
       </form>
     </div>
