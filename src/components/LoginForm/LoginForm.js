4c1ec6bbf5eaa3820311c7a777c22c412134cdfb
KasyanAndrey <andreykasyan88@gmail.com>
2021-12-14 09:38:09 +0200

    change css/cheng LoginForm

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,18 +1,66 @@
 import s from "./LoginForm.module.css";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { register, logIn } from "../../redux/auth/operations";
 
-const LoginForm = () => {
+function LoginForm() {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginActive, setLoginActive] = useState(true);
+  const [registerActive, setRisterActive] = useState(false);
+
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case "email":
+        return setEmail(value);
+      case "password":
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
+
+  const toggleLoginBtn = () => {
+    setLoginActive(true);
+    setRisterActive(false);
+  };
+
+  const toggleRegisterBtn = () => {
+    setLoginActive(false);
+    setRisterActive(true);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (loginActive) {
+      dispatch(logIn({ email, password }));
+      setEmail("");
+      setPassword("");
+    }
+
+    if (registerActive) {
+      dispatch(register({ email, password }));
+      setEmail("");
+      setPassword("");
+    }
+  }
+
   return (
     <div className={s.wrapper}>
-      <form className={s.form} onSubmit>
+      <form className={s.form} onSubmit={handleSubmit} autoComplete="off">
         <label className={s.label}>
           <p className={s.description}>Электронная почта:</p>
           <input
             className={s.input}
-            type="text"
+            type="email"
             name="email"
+            value={email}
             required
-            value
-            onChange
+            onChange={handleChange}
+            placeholder="@mail"
           />
         </label>
         <label className={s.label}>
@@ -21,24 +69,36 @@ const LoginForm = () => {
             className={s.input}
             type="password"
             name="password"
+            value={password}
             required
-            value
-            onChange
-            autoComplete="off"
+            onChange={handleChange}
+            placeholder="**********"
           />
         </label>
 
         <div className={s.formButton}>
-          <button className={s.button} type="submit">
+          <button
+            className={s.button}
+            type="submit"
+            onClick={toggleLoginBtn}
+            active={loginActive.toString()}
+            name="login"
+          >
             Войти
           </button>
-          <button className={s.button} type="submit">
+          <button
+            className={s.button}
+            type="submit"
+            onClick={toggleRegisterBtn}
+            active={registerActive.toString()}
+            name="register"
+          >
             Регистрация
           </button>
         </div>
       </form>
     </div>
   );
-};
+}
 
 export default LoginForm;
--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,37 +1,37 @@
 import React from "react";
 import s from "./Login.module.css";
 import LoginForm from "../../components/LoginForm/LoginForm";
-import back2 from "../../assets/images/back2.png";
 
 function Login() {
   return (
-    <div className={s.wrapper}>
-      <img className={s.bgImg} title="back" src={back2} alt="back" />
-      <span className={s.kapustaImg}></span>
-      <span className={s.kapustaImg2}></span>
-      <div className={s.loginPage}>
-        <div className={s.titleBox}>
-          <h1 className={s.title}>
-            Kapu<span></span>ta
-          </h1>
-          <p className={s.titleDescription}>Smart finance</p>
-        </div>
-        <div className={s.formBox}>
-          <p className={s.descriptionA}>
-            Вы можете авторизоваться с помощью Google Account:
-          </p>
-          <button className={s.button} type="button">
-            <span></span>
-            Google
-          </button>
-          <p className={s.descriptionB}>
-            Или зайти с помощью e-mail и пароля, предварительно
-            зарегистрировавшись:
-          </p>
-          <LoginForm />
+    <section className={s.section}>
+      <div className={s.container}>
+        <div className={s.kapImg}></div>
+        <div className={s.kapImg2}></div>
+        <div className={s.loginPage}>
+          <div className={s.titleBox}>
+            <h1 className={s.title}>
+              Kapu<span></span>ta
+            </h1>
+            <p className={s.titleDescription}>Smart finance</p>
+          </div>
+          <div className={s.formBox}>
+            <p className={s.descriptionA}>
+              Вы можете авторизоваться с помощью Google Account:
+            </p>
+            <button className={s.button} type="button">
+              <span></span>
+              Google
+            </button>
+            <p className={s.descriptionB}>
+              Или зайти с помощью e-mail и пароля, предварительно
+              зарегистрировавшись:
+            </p>
+            <LoginForm />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
