d7d69f583071fa66c716ef967ad85089ee5c377d
Logvina <irinlogvina9@gmail.com>
2021-12-16 00:31:24 +0200

    1

---
+++ src/components/CurrentAmount/CurrentAmount.js
@@ -0,0 +1,20 @@
+
+import s from "./CurrentAmount.module.css";
+import Line from "../../assets/images/line.svg";
+
+const CurrentAmount = () => {
+  return (
+    <div className={s.userAmount}>
+      <div className={s.expensesWrap}>
+        <p className={s.expenseText}>Расходы :</p>
+        <span className={s.expensesTotal}>-100 грн.</span>
+      </div>
+      <img className={s.Line} src={Line} alt="line" width="2" height="32" />
+      <div className={s.incomeWrap}>
+        <p className={s.incomeText}>Доходы :</p>
+        <span className={s.incomeTotal}>+1000000 грн.</span>
+      </div>
+    </div>
+  );
+};
+export default CurrentAmount;
