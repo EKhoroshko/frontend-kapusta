09e6537fda61619a047feb9ec13a94c738d6d708
EKhoroshko <xapoxa14@gmail.com>
2021-12-17 15:05:32 +0200

    listselectaCasts

--- src/helpers/Select/SelectList.js
+++ src/helpers/Select/SelectList.js
@@ -1,7 +1,15 @@
 const options = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
+  { value: "Транспорт", label: "Транспорт" },
+  { value: "Продукты", label: "Продукты" },
+  { value: "Здоровье", label: "Здоровье" },
+  { value: "Алкоголь", label: "Алкоголь" },
+  { value: "Развлечения", label: "Развлечения" },
+  { value: "Все для дома", label: "Все для дома" },
+  { value: "Техника", label: "Техника" },
+  { value: "Коммуналка, связь", label: "Коммуналка, связь" },
+  { value: "Спорт, хобби", label: "Спорт, хобби" },
+  { value: "Образование", label: "Образование" },
+  { value: "Прочее", label: "Прочее" },
 ];
 
 export default options;
