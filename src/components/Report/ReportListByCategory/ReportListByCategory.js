79181580ff9f406a15048edd839502150a99e114
volodymyr-oliinyk8 <oliynyk.vo@gmail.com>
2021-12-18 20:47:13 +0200

    completed markup category

--- src/components/Report/ReportListByCategory/ReportListByCategory.js
+++ src/components/Report/ReportListByCategory/ReportListByCategory.js
@@ -1,15 +1,30 @@
 import React from "react";
 import ReportItemByCategory from "../ReportItemByCategory";
+import { ReactComponent as BtnLeft } from "../../../assets/images/BtnLefl.svg";
+import { ReactComponent as BtnRight } from "../../../assets/images/BtnRight.svg";
 
-const ReportListByCategory = () => {
+import styles from "./ReportListByCategory.module.css";
+
+const ReportListByCategory = ({ items }) => {
   return (
-    <div>
-      <ul>
-        <li key="тут id елемента">
-          <button type="button" onClick={() => {}}>
-            <ReportItemByCategory />
-          </button>
-        </li>
+    <div className={styles.container}>
+      <div className={styles.switchWrap}>
+        <button type="button" name="leftBtn" className={styles.calendarBtn}>
+          <BtnLeft />
+        </button>
+        <p className={styles.cldrMonth}>Расх/прих</p>
+        <button type="button" name="rightBtn" className={styles.calendarBtn}>
+          <BtnRight />
+        </button>
+      </div>
+      <ul className={styles.list}>
+        {items.map(({ name, amount, icon, id }) => (
+          <li className={styles.listItem} key={id}>
+            <button type="button">
+              <ReportItemByCategory name={name} amount={amount} icon={icon} />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
