a84a79497ded291eb3798294815899f384f3f13c
Mariia Bondar <param.pam.pam.mariia@gmail.com>
2021-12-19 00:47:27 +0200

    current period

--- src/components/Report/CurrentPeriod/CurrentPeriod.js
+++ src/components/Report/CurrentPeriod/CurrentPeriod.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getMonth, getYear } from "../../../redux/currentPeriod/selectors";
+//import { getMonth, getYear } from "../../../redux/currentPeriod/selectors";
 import { ReactComponent as VectorLeft } from "../../../assets/images/vector-left.svg";
 import { ReactComponent as VectorRight } from "../../../assets/images/vector-right.svg";
 import monthName from "../../../helpers/Current-period/months.json";
@@ -9,49 +9,61 @@ import periodStyles from "./Period.module.css";
 // я ще над цим працюю
 export default function Period() {
   const dispatch = useDispatch();
-  const [date, setDate] = useState("");
-  const month = useSelector(getMonth);
-  const year = useSelector(getYear);
 
+  //const [date, setDate] = useState("");
   const currentMonth = new Date().getMonth() + 1;
   const currentYear = new Date().getFullYear();
+  const [month, setMonth] = useState(currentMonth);
+  const [year, setYear] = useState(currentYear);
+  // const month = useSelector(getMonth);
+  // const year = useSelector(getYear);
+
   const getMonthName = monthName.filter((el) => el.id === String(currentMonth));
 
   const handleNextMonthButtonClick = () => {
-    const newDate = new Date(month + 1, year);
-
-    setDate({ newDate });
+    if (month <= 12) {
+      setMonth((prev) => (prev += 1));
+    } else {
+      setMonth(1);
+      setYear((prev) => (prev += 1));
+    }
   };
 
   const handlePrevMonthButtonClick = () => {
-    const newDate = (month - 1, year);
-    setDate({ newDate });
+    // const newDate = (month - 1, year);
+    // setDate({ newDate });
+    if (month <= 1) {
+      setMonth(12);
+      setYear((prev) => (prev -= 1));
+    } else {
+      setMonth((prev) => (prev -= 1));
+    }
   };
 
   return (
     <div className={periodStyles.container}>
       <p className={periodStyles.text}>Текущий период:</p>
-      <button
-        type="button"
-        className={periodStyles.NavButton}
-        aria-label="previous"
-        onClick={handlePrevMonthButtonClick}
-      >
-        <VectorLeft width="7" height="12" />
-      </button>
-
-      <p
-        className={periodStyles.CurrentPeriod}
-      >{`${getMonthName[0].name} ${currentYear}`}</p>
-
-      <button
-        type="button"
-        className={periodStyles.NavButton}
-        aria-label="next"
-        onClick={handleNextMonthButtonClick}
-      >
-        <VectorRight width="7" height="12" />
-      </button>
+      <div className={periodStyles.period}>
+        <button
+          type="button"
+          className={periodStyles.NavButton}
+          aria-label="previous"
+          onClick={handlePrevMonthButtonClick}
+        >
+          <VectorLeft width="7" height="12" />
+        </button>
+        <p className={periodStyles.CurrentPeriod}>
+          {`${getMonthName[0].name} ${currentYear}`}
+        </p>
+        <button
+          type="button"
+          className={periodStyles.NavButton}
+          aria-label="next"
+          onClick={handleNextMonthButtonClick}
+        >
+          <VectorRight width="7" height="12" />
+        </button>
+      </div>
     </div>
   );
 }
