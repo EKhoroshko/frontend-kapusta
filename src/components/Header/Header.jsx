7e964c8642f62a8c5c00d6d83e8e75fe0e69c0fb
Wlad <wladkrychin@gmail.com>
2021-12-22 18:09:41 +0100

    goHome

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,15 @@ import UserMenu from "./userMenu";
 import logo from "../../assets/images/logo.svg";
 import { useSelector } from "react-redux";
 import { getIsLoggedIn } from "../../redux/auth/selectors";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 function Header() {
   const isLoggedIn = useSelector(getIsLoggedIn);
   return (
     <header className={css.header}>
       <div className={css.container}>
-        <img src={logo} alt="" width={90} height={30} />
+        <Link to={isLoggedIn ? "/home" : "/"}>
+          <img src={logo} alt="" width={90} height={30} />
+        </Link>
         {isLoggedIn && <UserMenu />}
         {!isLoggedIn && <Redirect to="/" />}
       </div>
