c748046bf40eabc751464ebf9d03d12f09646774
Mariia Bondar <param.pam.pam.mariia@gmail.com>
2021-12-23 16:14:26 +0200

    css

--- src/routes/Veryfy/Veryfy.js
+++ src/routes/Veryfy/Veryfy.js
@@ -5,6 +5,7 @@ import { sliceToken } from "../../redux/auth/slice";
 import { useLocation } from "react-router-dom";
 import { veryfication } from "../../redux/auth/operations";
 import { getVerify } from "../../redux/auth/selectors";
+import s from "./Veryfy.module.css";
 
 function Veryfy() {
   const varify = useSelector(getVerify);
@@ -24,13 +25,15 @@ function Veryfy() {
 
   // const Bounce = styled.div`animation: 2s ${keyframes`${bounce}`} infinite`;
   return (
-    <div>
+    <div className={s.Veryfy}>
       {varify ? (
-        <h3>Вы успешно прошли верификацию</h3>
+        <h3 className={s.text}>Вы успешно прошли верификацию</h3>
       ) : (
-        <h3>Мы проверяем ваш email подождите...</h3>
+        <h3 className={s.text}>Мы проверяем ваш email подождите...</h3>
       )}
-      <Link to={"/"}>Перейти к логинизации</Link>
+      <Link to={"/"} className={s.link}>
+        Перейти к логинизации
+      </Link>
     </div>
   );
 }
