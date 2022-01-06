00ef37d67401fa0766f8bcde51c2648de41239e7
EKhoroshko <xapoxa14@gmail.com>
2022-01-06 04:04:30 +0200

    clearfolder

--- src/redux/currentPeriod/actions.js
+++
@@ -1,4 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-export const nextPeriod = createAction("currentPeriod/nextPeriod");
-export const previousPeriod = createAction("currentPeriod/previousPeriod");
--- src/redux/currentPeriod/selectors.js
+++
@@ -1,2 +0,0 @@
-export const getMonth = (state) => state.currentPeriod.getMonth;
-export const getYear = (state) => state.currentPeriod.getFullYear;
