0684014984fd3ca441af973413d0e0574d13a740
EKhoroshko <xapoxa14@gmail.com>
2022-01-17 01:38:20 +0200

    clear

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./Loader.module.css";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
-// import LoaderComponent from "react-loader-spinner";
 
 export default function Skeleton() {
   return (
@@ -12,7 +11,7 @@ export default function Skeleton() {
         color="#00BFFF"
         height={100}
         width={100}
-        timeout={3000} //3 secs
+        timeout={3000}
       />
     </div>
   );
--- src/routes/NotFound/NotFound.js
+++ src/routes/NotFound/NotFound.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import css from "./NotFound.module.css";
 
 function NotFound() {
   return <div>notFound</div>;
